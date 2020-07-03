1574b4dbaa5b71748d79ac86fb495afc06ff109b
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-07-04 00:06:47 +1000

    Create vectorsummation.cc

---
+++ vectorsummation.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int n = 40;
+
+//sum and return elements of d
+inline void sum(int& p, vector<int> d) {
+    p = 0;
+
+    for (int i = 0; i < n; ++i) {
+        p = p + d[i];
+    }
+}
+
+int main() {
+    int accum = 0;
+    vector<int> data;
+
+    //set each value of the vector data to its index
+    for (int i = 0; i < n; ++i) {
+        data.push_back(i);
+    }
+
+    //invoke sum and output result
+    sum(accum, data);
+    cout << "The sum is: " << accum << endl;
+
+    return 0;
+}
