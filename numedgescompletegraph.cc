44a4024ceb8e5a8e5198f128127ac9aff40b1e0e
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-07-04 15:35:44 +1000

    Create numedgescompletegraph.cc

---
+++ numedgescompletegraph.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+//calculate number of edges of a
+//complete graph, given the number
+//of nodes. 
+inline int num_edges(int n) {
+    if (n == 1)
+        return n;
+
+    return num_edges(n - 1) + n;
+}
+
+
+int main() {
+
+    //number of edges per node is 
+    //number of nodes - 1
+    int n = 4;
+    int enode = n - 1;
+
+    cout << num_edges(enode) << endl;
+
+    return 0;
+}
