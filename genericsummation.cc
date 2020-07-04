03bcd06010136577558b6f907e07a1d70888ef90
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-07-04 14:21:42 +1000

    Update genericsummation.cc

--- genericsummation.cc
+++ genericsummation.cc
@@ -1,26 +1,31 @@
 #include <iostream>
+#include <vector>
+
 using std::cout;
 using std::cin;
 using std::endl;
-
+using std::vector;
 
 template <class T>
-T inline summation(T d[], int n) {
-    T sum;
+T sum(vector<T> d, int size) {
+    T res = 0;
 
-    for (int i = 0; i < n; ++i) {
-        sum += d[i];
+    for (int i = 0; i < size; ++i) {
+        res += d[i];
     }
 
-    return sum;
+    return res;
 }
 
+int main() {
+    const int size = 4;
+    vector<int> d1;
 
+    for (int i = 0; i < size; ++i) {
+        d1.push_back(i);
+    }
 
-int main(void) {
-    double a_list[] = { 3.4, 3.5, 1.2, 5 };
-    int b_list[] = { 1, 2, 3, 4 };
+    cout << sum(d1, size) << endl;
 
-    cout << summation(a_list, 4) << endl;
-    cout << summation(b_list, 4) << endl;
+    return 0;
 }
