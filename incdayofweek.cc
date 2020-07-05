35bebaee4b3d307ae1e677e573e02888484c3273
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-07-05 20:59:01 +1000

    Update incdayofweek.cc

--- incdayofweek.cc
+++ incdayofweek.cc
@@ -3,6 +3,7 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::ostream;
 
 typedef enum days{SUN, MON, TUES, WEDS, THURS, FRI, SAT} days;
 
@@ -12,6 +13,20 @@ inline days operator++ (days d) {
 
 inline days operator-- (days d) {
     return static_cast<days>((static_cast<int>(d) + 6) % 7);
+}
+
+ostream& operator<< (ostream& out, const days& d) {
+    switch(d) {
+        case SUN: out << "SUN"; break;
+        case MON: out << "MON"; break;
+        case TUES: out << "TUES"; break;
+        case WEDS: out << "WEDS"; break;
+        case THURS: out << "THURS"; break;
+        case FRI: out << "FRI"; break;
+        case SAT: out << "SAT"; break;
+    }
+
+    return out;
 }
 
 int main() {
